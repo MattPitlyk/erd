5c64d75bcf9d63cc08192431ca8a21af79cf558d
MattPitlyk <matt.pitlyk@gmail.com>
2019-03-08 11:49:43 -0600

    Update examples.py

--- examples.py
+++ examples.py
@@ -5,11 +5,7 @@ Each inner tuple is Tuple[table_name, field_name] that IDs a field in a
 table. In each pair of tuples, the the first is the source field and the
 second is the destination field."""
 
-
 import erd
-
-
-
 
 if __name__=='__main__':
     mapping_example = [(('Table A', 'Field 1'), ('Table B', 'Field 4')),
